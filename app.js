e2074f82f5fd37a881416581f3fa8699d62f8cfe
Manoj Nama <manoj.nama@outlook.com>
2016-03-26 11:52:04 +0530

    Added Procfile for heroku deployment

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var express = require("express"),
 
 app.use(express.static("./app"));
 
-app.listen(3000, "0.0.0.0", function () {
+app.listen(process.env.PORT || 3000, "0.0.0.0", function () {
 	console.log("Server listening on port 3000");
 });
